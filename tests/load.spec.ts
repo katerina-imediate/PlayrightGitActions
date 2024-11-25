408178363271543556bb278ff6eef939237a3f65
katerina-imediate <katexrg@yahoo.com>
2024-11-25 12:31:21 +0200

    small chnages

--- tests/load.spec.ts
+++ tests/load.spec.ts
@@ -6,6 +6,10 @@ for (let i = 0; i < 10; ++i) {
     tag: '@fast',
   }, async ({ playwright, page, }) => {
     const browser = await playwright.chromium.launch();
+    // await page.route('**', route => {
+    //   console.log(route.request().url());
+    //   route.continue();
+    // });
     try {
       await page.goto('https://playwright.dev/');
       await expect(page.getByRole('link', { name: 'Get started' })).toBeVisible();
