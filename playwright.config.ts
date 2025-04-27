d1a97d9ae9519e108b9f1a3122720b928c8c3617
katerina-imediate <katexrg@yahoo.com>
2025-04-27 10:23:45 +0300

    list reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["blob"], ["github"]] : [["html"]],
+  reporter: process.env.CI ? [["list"], ["github"]] : [["html"]],
   //grepInvert: /.*example.spec.ts/,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
